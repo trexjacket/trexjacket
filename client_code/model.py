ff8824040891672fbbd0f1b17837af1819fcdc5d
Owen Campbell <owen.campbell@empiria.co.uk>
2022-03-07 10:55:12 +0000

    Initial reworking of marks by Dan

--- client_code/model.py
+++ client_code/model.py
@@ -14,6 +14,128 @@ class Dimension:
     def __repr__(self):
         return f"Dimension(name={self.name!r}, value={self.value!r})"
 
+    def __hash__(self):
+        return hash((self.name, self.value))
+
+    def __eq__(self, other):
+        return self.name == other.name and self.value == other.value
+
+
+class DansMark:
+    """Basically a class for Dan to throw some spaghetti to see what sticks
+
+    Usage:
+    mark.dimensions: a tuple of the identifying dimensions
+    mark.identifier: a tuple identifying the mark
+    mark.measures: a tuple of the underlying data
+    mark.values:  a tuple of the actual data that is associated with this mark, mapped to measures
+
+    Marks are accessible by case-insensitive dictionary lookup
+    i.e. a mark can be accessed mark['profit'] and this will return the value associated with any of:
+      mark measure SUM(Profit)
+      mark measure (Profit)
+    """
+
+    def __init__(self, dimensions):
+        self.values_dict = dict()
+        self.dimensions = dimensions
+
+    @property
+    def identifier(self):
+        return tuple([d.value for d in self.dimensions])
+
+    @property
+    def identifying_properties(self):
+        return (d.name for d in self.dimensions)
+
+    @property
+    def value(self):
+        return self.values_dict.values[0]
+
+    @property
+    def values(self):
+        return self.values_dict.values
+
+    @property
+    def measures(self):
+        return list(self.values_dict.keys())
+
+    def __getitem__(self, key):
+        return self.values_dict[_clean_record_key(key)]
+
+    def get(self, key):
+        return self.values_dict.get(_clean_record_key(key))
+
+    def __repr__(self):
+        return f"DansMark: Identified by {self.dimensions}, values: {self.values_dict}"
+
+
+def generate_dans_marks(records):
+    """Generates Dans Marks"""
+    # we iterate through all records.
+    # Some "Marks" can be assembled from multiple records if they share a common identifier (frozen set of dimensions)
+    # This is not really known until we have iterated through all records
+    # We do rely on a record having a complete set of dimensions, or they will be assembled separately (i.e. dimensions
+    # must fully overlap for us to group them)
+    dans_marks = dict()
+    for record in records:
+        all_dimensions = set()
+        all_values = dict()
+        measure_flag = False
+        for key, value in record.items():
+            this_value = None
+            this_dimension = None
+            aggregation_match = aggregation_pattern.search(key)
+            datetime_match = datetime_pattern.search(key)
+            category_match = isinstance(value, str)
+            is_measure_name = key == "measure_names"
+            is_measure_value = key == "measure_values"
+
+            if is_measure_name:
+                measure_flag = True
+                measure_name = value
+
+            elif is_measure_value:
+                measure_flag = True
+                measure_value = value
+
+            elif datetime_match:
+                name = datetime_match.group(2)
+                this_dimension = Dimension(name, value)
+
+            elif category_match:
+                this_dimension = Dimension(key, value)
+
+            elif aggregation_match:
+                name = aggregation_match.group(2)
+                aggregation = aggregation_match.group(
+                    1
+                )  # for now we don't use this since my model is too basic
+                this_value = {_clean_record_key(name): value}
+
+            else:
+                this_value = {_clean_record_key(key): value}
+
+            if this_dimension:
+                all_dimensions.add(this_dimension)
+            elif this_value:
+                all_values.update(this_value)
+
+        if measure_flag:
+            all_values[_clean_record_key(measure_name)] = measure_value
+
+        all_dimensions = frozenset(all_dimensions)
+
+        if all_dimensions in dans_marks:
+            dans_marks[all_dimensions].values_dict.update(all_values)
+
+        else:
+            new_mark = DansMark(all_dimensions)
+            new_mark.values_dict = all_values
+            dans_marks[all_dimensions] = new_mark
+
+    return list(dans_marks.values())
+
 
 class Mark:
     """Wrapper for the data source of a selected mark
@@ -119,6 +241,7 @@ def _marks(record):
 
     for mark in result.values():
         mark.dimensions = dimensions
+
     return result
 
 
@@ -164,8 +287,14 @@ class DataTable(TableauProxy):
         keys = [c.fieldName for c in self._proxy.columns]
         return [
             {
-                _clean_record_key(attr[0]): attr[1]
-                for attr in zip(keys, [data_value.nativeValue for data_value in row])
+                _clean_record_key(attr[0]): (
+                    attr[1] if attr[0] != "Measure Names" else attr[2]
+                )
+                for attr in zip(
+                    keys,
+                    [data_value.nativeValue for data_value in row],
+                    [data_value.formattedValue for data_value in row],
+                )
             }
             for row in self._proxy.data
         ]
@@ -229,11 +358,18 @@ class Worksheet(TableauProxy):
         return list(itertools.chain(*[dt.records for dt in datatables]))
 
     @property
-    def marks(self):
+    def selected_marks(self):
         data = self._proxy.getSelectedMarksAsync()["data"]
         datatables = (DataTable(table) for table in data)
         records = list(itertools.chain(*[dt.records for dt in datatables]))
         return marks_collection(records)
+
+    @property
+    def selected_dans_marks(self):
+        data = self._proxy.getSelectedMarksAsync()["data"]
+        datatables = (DataTable(table) for table in data)
+        records = list(itertools.chain(*[dt.records for dt in datatables]))
+        return generate_dans_marks(records)
 
     @property
     def filters(self):
@@ -283,6 +419,12 @@ class Dashboard:
         if self.proxy is None:
             return []
         return [Parameter(p) for p in self.proxy.getParametersAsync()]
+
+    @property
+    def name(self):
+        if self.proxy is None:
+            return None
+        return self.proxy.name
 
     def refresh_data_sources(self):
         data_sources_generator = (ws.data_sources for ws in self.worksheets)

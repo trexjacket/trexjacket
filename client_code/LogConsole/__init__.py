9b59ec251da7725cf32a8e46405b0151aadc5b77
Owen Campbell <owen.campbell@empiria.co.uk>
2022-03-14 12:46:59 +0000

    Add filter_changed and parameter_changed default handlers

--- client_code/LogConsole/__init__.py
+++ client_code/LogConsole/__init__.py
@@ -5,9 +5,10 @@ from ._anvil_designer import LogConsoleTemplate
 class LogConsole(LogConsoleTemplate):
     def __init__(self, **properties):
         self.session = Tableau.session()
-        self.session.dashboard.register_event_handler(
-            "selection_changed", self._on_selection_change
-        )
+        dashboard = self.session.dashboard
+        dashboard.register_event_handler("filter_changed", self._on_filter_change)
+        dashboard.register_event_handler("parameter_changed", self._on_parameter_change)
+        dashboard.register_event_handler("selection_changed", self._on_selection_change)
         self.init_components(**properties)
 
     def form_show(self, **event_args):
@@ -18,6 +19,22 @@ class LogConsole(LogConsoleTemplate):
 
     def button_clear_click(self, **event_args):
         self.logging_console.text = ""
+
+    def _on_filter_change(self, event):
+        logger = self.session.logger
+        logger.log("*" * 50)
+        logger.log("Filter Change detected...")
+        logger.log(f"** Field Name: {event.filter.field_name}")
+        logger.log(f"** Values: {event.filter.applied_values}")
+        logger.log("*" * 50)
+
+    def _on_parameter_change(self, event):
+        logger = self.session.logger
+        logger.log("*" * 50)
+        logger.log("Parameter Change detected...")
+        logger.log(f"** Parameter name: {event.parameter.name}")
+        logger.log(f"** Parameter value: {event.parameter.value}")
+        logger.log("*" * 50)
 
     def _on_selection_change(self, event):
         logger = self.session.logger

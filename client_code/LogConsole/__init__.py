ab006f4e9fb4eacb4ed83feb9f0fad2c44711b70
danbolinson-bt <daniel.bolinson@bakertilly.com>
2022-05-15 23:19:40 -0500

    Adds componenet parameters for default logger behavior

--- client_code/LogConsole/__init__.py
+++ client_code/LogConsole/__init__.py
@@ -1,12 +1,28 @@
+
 from .._logging import register_default_handlers
 from ..model.proxies import Tableau
+from ..model.events import SELECTION_CHANGED, FILTER_CHANGED, PARAMETER_CHANGED
 from ._anvil_designer import LogConsoleTemplate
+
 
 
 class LogConsole(LogConsoleTemplate):
     def __init__(self, **properties):
         self.session = Tableau.session()
-        register_default_handlers(self.session)
+        
+        # Establish default behavior per component properties
+        event_types = []
+        if self.log_filter_changed:
+            event_types.append(FILTER_CHANGED)
+        if self.log_parameter_changed:
+            event_types.append(PARAMETER_CHANGED)
+        if self.log_selection_changed:
+            event_types.append(SELECTION_CHANGED)
+        
+        print(f"Registering logger with event handlers: {event_types}. Printing to console: {self.log_to_console}")
+        register_default_handlers(self.session, event_types)
+        self.session.logger.with_anvil_logging = self.log_to_console
+        
         self.init_components(**properties)
 
     def form_show(self, **event_args):
@@ -17,3 +33,6 @@ class LogConsole(LogConsoleTemplate):
 
     def button_clear_click(self, **event_args):
         self.logging_console.text = ""
+
+    def _refresh_bindings(self, **event_args):
+      self.refresh_data_bindings()

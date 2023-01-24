1845993455dba81aabce194e56359621754100cc
ayoskovich-bt <102990575+ayoskovich-bt@users.noreply.github.com>
2023-01-24 11:15:57 -0500

    Creates a get_environment function (#131)
    * Creates a get_environment function
    
    * Update client_code/api.py

--- client_code/api.py
+++ client_code/api.py
@@ -6,6 +6,40 @@ from .model.proxies import _Tableau
 __version__ = "0.2"
 
 
+def get_environment():
+    """Returns a dictionary of useful information about the environment the extension is currently running in.
+
+    See a description of each here: https://tableau.github.io/extensions-api/docs/interfaces/environment.html#apiversion
+
+    Returns
+    -------
+
+    :obj:`dict`
+      A dictionary containing items such as the Tableau API version, Tableau version, operating system,
+      and others.
+
+    Example
+    -------
+    >>> from tableau_extensions import api
+    >>> print(api.get_environment())
+    {'apiVersion': '1.9.0', 'context': 'desktop', 'country': None, 'language': 'en', 'locale': 'en-us', 'mode': 'authoring', 'operatingSystem': 'win', 'tableauVersion': '2022.1.8'}
+    """
+    from anvil import tableau
+
+    env = tableau.extensions.environment
+    attrs = [
+        "apiVersion",
+        "context",
+        "country",
+        "language",
+        "locale",
+        "mode",
+        "operatingSystem",
+        "tableauVersion",
+    ]
+    return {attr: env.get(attr) for attr in attrs}
+
+
 def get_dashboard():
     """Gets the instance of Tableau Dashboard that represents the current connection. This is
     the recommended entry point to the Tableau Extensions API.
@@ -17,5 +51,10 @@ def get_dashboard():
         The Dashboard instance associated with the current session.
         It is recommended that this is the only way you 'get' the current dashboard,
         since it is an attribute of Session, which deals with a bunch of house-keeping.
+
+    Example
+    -------
+    >>> from tableau_extensions import api
+    >>> mydashboard = api.get_dashboard()
     """
     return _Tableau.session().dashboard
